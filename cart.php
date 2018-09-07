14d69e9bbe8beeb64fb2679afceae6a4f8f9b0b4
Alexandro <39122357+Svin228@users.noreply.github.com>
2018-09-07 21:24:25 +0300

    fix errors array

--- cart.php
+++ cart.php
@@ -5,8 +5,33 @@ require 'goods.php';
 if(!isset($_SESSION['cart'])){
 	$_SESSION['cart']=['sum'=> 0,'items'=>[ ]];	
 }
-check();
+$errors = [];
+$products = getProducts();
+if(!empty($_POST)){
+
+    if(isset($_POST['goods'])&& $_POST['goods']!=0){
+        $product = $_POST['goods'];
+    }else{
+        $errors['goods'] ='Выберите товар';
+    }
+    if(isset($_POST['count'])&&$_POST['count']!=0){
+        $count = $_POST['count'];
+    }else{
+        $errors['count'] = 'Введите количество товара';
+    }
+    
+    if(empty($errors)){
+        $index = $product;
+        $product = $products[$index];
+        if(isset($_SESSION['cart']['items'][$index])){
+            $count +=  $_SESSION['cart']['items'][$index]['count'];
+        }
+    $_SESSION['cart']['items'][$index] = ['name'=> $product["name"],'count'=>$count];
+    }
+}
+//check();
 cartRecalc();
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -23,11 +48,15 @@ cartRecalc();
     <!-- <button class="change" onclick="changeTheme()">Change theme</button> -->
    
     <form action="" method="POST" class="form" enctype="multipart/form-data">
-    <?php if(isset($errors['goods'])) {
-	echo $errors['goods'];
+    <?php
+    if(isset($errors['goods'])) {
+	echo '<p class="trouble">'.$errors['goods'].'</p>';
+    }
+    if(isset($errors['count'])) {
+        echo '<p class="trouble">'.$errors['count'].'</p>';
     }?>
     <select name="goods" id="" class="input">
-        <option value="0">Выберите товар</option>
+        <option value="">Выберите товар</option>
         <option value="1">Футболка</option>
         <option value="2">Джинсы</option>
         <option value="3">Кроссовки</option>
@@ -35,16 +64,12 @@ cartRecalc();
         <option value="5">Рубашка</option>
     </select>
     <input type="count" name="count" class="input" placeholder="Количество">
-    <?php if(isset($errors['count'])) {
-	echo $errors['count'];
-    }?>
     <button type="submit" name="login" class="submit-btn">Посчитать</button>
     <?php
     foreach($_SESSION['cart']['items'] as $key=>$item){
         echo '<div class="output">'.$item['name'].'    -    '.$item['count'].' шт.    
         <a href="delete.php" id ="'.$key.'"><button type="submit">X</button></a></div><br>';
     }
-
     ?>
     <hr>
     <p>К оплате <?php echo  $_SESSION['cart']['sum'];?></p>
