ef40f3bcc9c7a4706e30537152828b5e991bda66
Alexandro <39122357+Svin228@users.noreply.github.com>
2018-09-07 20:44:14 +0300

    small fix

--- index.php
+++ index.php
@@ -3,16 +3,21 @@ session_start();
 
 require 'data.php';
 require 'functions.php';
-
-if(isset($_POST['login']) && isset($_POST['password'])){
-    if(verify_user($_POST['login'],$_POST['password'] ,$data)){
-        $_SESSION['Name'] = $_POST['login'];
-        $_SESSION['Password'] = $_POST['password'];
+$problems = [];
+if(!empty($_POST)){
+    if(isset($_POST['login']) && isset($_POST['password'])){
+        if(verify_user($_POST['login'],$_POST['password'] ,$data)){
+            $_SESSION['Name'] = $_POST['login'];
+            $_SESSION['Password'] = $_POST['password'];
+        }
     }
-}
 
-if(isset($_SESSION['Name']) && isset($_SESSION['Password'])){
-    header('Location: cart.php');
+    if(isset($_SESSION['Name']) && isset($_SESSION['Password'])){
+        header('Location: cart.php');
+    }
+    else {
+        $problems['trouble'] = 'Пароль или логин введен неверно';
+    }
 }
 ?>
 
@@ -29,7 +34,11 @@ if(isset($_SESSION['Name']) && isset($_SESSION['Password'])){
 <body>
     <h1 id="header">Authentication</h1>
     <!-- <button class="change" onclick="changeTheme()">Change theme</button> -->
+
     <form  method="POST" action="" class="form">
+    <?php if(isset($problems['trouble'])) {
+	echo '<p class="trouble">'.$problems['trouble'].'</p>';
+    }?>
     <input type="text" name="login" placeholder="olexandro" required="required" class="login">
     <input type="text" name="password" placeholder="olexandro" required="required" class="pass">
     <button type="submit" name="sign in" class="submit-btn" >Sign in</button>
